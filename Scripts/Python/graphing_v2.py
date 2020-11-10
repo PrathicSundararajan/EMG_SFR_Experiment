d1a2e2c38ddf80353897186335d9963e7a7c30ec
PrathicSundararajan <prathic44@gmail.com>
2020-11-10 00:50:56 -0800

    Added FFT Functinality & Modularized

--- Scripts/Python/graphing_v2.py
+++ Scripts/Python/graphing_v2.py
@@ -6,16 +6,14 @@
 # Done - apply filter
 # TODO - Confirm noise is being removed
 # TODO - Maybe consider building bar graph on python?
+# TODO - Add a README file
 
 import numpy as np
-import statistics
 from scipy import stats, signal, fft, ifft
 from scipy.fftpack import fftshift
 import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-#from scipy import
-
 
 
 def preset_params():
@@ -86,6 +84,7 @@ def reading_in_file(file_path):
 
     return local_point_id, local_total_micro, local_minutes, local_sec, local_val
 
+
 def adding_expected_markers():
     # plotting times for expected reaction
     markers_x = [5, 17, 29, 41, 53]
@@ -96,21 +95,22 @@ def adding_expected_markers():
         axs[1].axvline(markers_x[i], color='r', zorder=100)
         axs[2].axvline(markers_x_2[i], color='r', zorder=100)
 
+
 def data_processing(local_total_micro, local_val):
     # rectifying data
     local_rectified_1 = abs(local_val - sum(local_val) / len(local_val))
 
     # Binning the data
     local_total_milli = local_total_micro / 1000.0
-    BIN_SIZE = 200
-    num_of_bins1 = math.floor(local_total_milli[-1] / BIN_SIZE)
+    bin_size = 200
+    num_of_bins1 = math.floor(local_total_milli[-1] / bin_size)
     local_rectified_1_bins = []
     curr_local_val = 0  # curr milli second point
 
     for i in range(num_of_bins1):
         curr_sum = 0
         counter = 0
-        while local_total_milli[curr_local_val] < (i + 1) * BIN_SIZE:
+        while local_total_milli[curr_local_val] < (i + 1) * bin_size:
             curr_sum += local_rectified_1[curr_local_val]
             counter += 1
             curr_local_val += 1
@@ -121,26 +121,50 @@ def data_processing(local_total_micro, local_val):
     return local_rectified_1, local_rectified_1_bins, local_rectified1_zscore
 
 
-def plotting_signal_time_domain(sec, local_val, local_rectified_1, local_rectified_1_bins, local_rectified1_zscore):
+def plotting_signal_time_domain(local_axs, local_sec, local_val, local_rectified_1, local_rectified_1_bins, local_rectified1_zscore):
     # adding expected times of reaction
     adding_expected_markers()
 
     # plotting raw data
-    axs[0].plot(sec, local_val, zorder=1)
+    local_axs[0].plot(local_sec, local_val, zorder=1)
 
     # plotting rectified data
-    axs[1].plot(sec, local_rectified_1, zorder=1)
+    local_axs[1].plot(local_sec, local_rectified_1, zorder=1)
     print(len(local_rectified_1_bins), "Total Bins Created")
 
     # plotting z-score data
-    axs[2].plot(range(len(local_rectified_1_bins)), local_rectified1_zscore, zorder=1)
+    local_axs[2].plot(range(len(local_rectified_1_bins)), local_rectified1_zscore, zorder=1)
 
     # setting axis labels
-    axs[0].set(ylabel="Raw (mV)", xlabel="Seconds", title="Raw EMG Recording")
-    axs[1].set(ylabel="Rectified (mV)", xlabel="Seconds", title="Rectified EMG Recording")
-    axs[2].set(ylabel="Z-Score", xlabel="Bin Number (Window Size: 200ms)", title="Facial Reactivity (Binned Z-Score) of Recording")
+    local_axs[0].set(ylabel="Raw (mV)", xlabel="Seconds", title="Raw EMG Recording")
+    local_axs[1].set(ylabel="Rectified (mV)", xlabel="Seconds", title="Rectified EMG Recording")
+    local_axs[2].set(ylabel="Z-Score", xlabel="Bin Number (Window Size: 200ms)", title="Facial Reactivity (Binned Z-Score) of Recording")
 
-# def fft_graphing():
+
+# FFT CODE: https://www.gaussianwaves.com/2020/01/how-to-plot-fft-in-python-fft-of-basic-signals-sine-and-cosine-waves/
+def fft_graphing(local_axs, local_subplot_pos, local_val, local_rectified1_zscore, local_color):
+    # now for fft
+    fs = math.ceil(len(local_val) / 60)
+    nfft = len(local_val)
+    x2 = fftshift(fft(local_rectified1_zscore, nfft))
+    f_vals = np.arange(start=-nfft / 2, stop=nfft / 2) * fs / nfft
+    half_start = math.floor(len(f_vals) / 2)
+    half_end = len(f_vals)
+    local_axs[0].plot(f_vals[half_start:half_end], np.abs(x2[half_start:half_end]), color=local_color)
+    local_axs[local_subplot_pos].plot(f_vals[half_start:half_end], np.abs(x2[half_start:half_end]), color=local_color)
+    # this is for magnitude spectrum
+    # local_axs[local_subplot_pos + 1].magnitude_spectrum(local_rectified1_zscore, color=local_color)
+
+
+def notch_filter(local_val):
+    fs = math.ceil(len(local_val) / 60)
+    print('Our frequency is', fs)
+    w0 = 60
+    q0 = 5
+    b, a = signal.iirnotch(w0, q0, fs)
+    filtered_data = signal.lfilter(b, a, local_val)
+    return filtered_data
+
 
 if __name__ == '__main__':
     # setting params for subplot graphing
@@ -152,12 +176,12 @@ if __name__ == '__main__':
     person_reading_name, files_read = selecting_files_read(person_reading_pos)
     files_read = ['CDP1_MM.csv']
 
-
-
+    # working through all the files that are to be looked at
     for x in range(len(files_read)):
         # reading in the data & pre-processing
         print(folder_name + files_read[x])
         point_id, total_micro, minutes, sec, val = reading_in_file(folder_name + files_read[x])
+
         # plotting with pyplot
         fig, axs = plt.subplots(3)
 
@@ -165,75 +189,29 @@ if __name__ == '__main__':
         rectified_1, rectified_1_bins, rectified1_zscore = data_processing(total_micro, val)
 
         # plotting raw, rectified & z-score signal
-        plotting_signal_time_domain(sec, val, rectified_1, rectified_1_bins, rectified1_zscore)
+        plotting_signal_time_domain(axs, sec, val, rectified_1, rectified_1_bins, rectified1_zscore)
 
         # fig.savefig(folder_name + 'Pictures/' +  person_reading_name + '/' + files_read[x][0:len(files_read[x]) - 4])
         # print('Saved fig: ', files_read[x])
 
-        #plt.figure()  # now for fft
-        fig2, axs2 = plt.subplots(1)
-
-        # sample spacing
-        # FFT CODE: https://www.gaussianwaves.com/2020/01/how-to-plot-fft-in-python-fft-of-basic-signals-sine-and-cosine-waves/
-        fs = math.ceil(len(val) / 60)
-        NFFT = len(val)
-        yf = fft(rectified1_zscore)
-        X = fftshift(fft(rectified1_zscore, NFFT))
-        fVals = np.arange(start=-NFFT / 2, stop=NFFT / 2) * fs / NFFT
-        half_start = math.floor(len(fVals)/2)
-        half_end = len(fVals)
-        axs2.plot(fVals[half_start:half_end], np.abs(X[half_start:half_end]))
-
-
+        # now for fft
+        fig2, axs2 = plt.subplots(3)
+        suplot_plot_pos = 1
+        fft_graphing(axs2, suplot_plot_pos, val, rectified1_zscore, 'red')
 
         # apply notch filter (60Hz)
+        filtered_val = notch_filter(val)
 
-        fs = math.ceil(len(val) / 60)
-        print('Our frequency is', fs)
-        #print(fs)
-        w0 = 60
-        Q0 = 5
-        b, a = signal.iirnotch(w0, Q0, fs)
-        #print(b)
-        #print(a)
-        filtered_data = signal.lfilter(b, a, val)
-        #print(filtered_data)
-        rectified_1 = filtered_data
-        total_milli = total_micro / 1000.0
-        BIN_SIZE = 200
-        num_of_bins1 = math.floor(total_milli[-1] / BIN_SIZE)
-        rectified_1_bins = []
-        curr_val = 0  # curr milli second point
+        # processing filtered data
+        filtered_rectified_1, filtered_rectified_1_bins, filtered_rectified1_zscore = data_processing(total_micro, filtered_val)
 
-        for i in range(num_of_bins1):
-            curr_sum = 0
-            counter = 0
-            while total_milli[curr_val] < (i + 1) * BIN_SIZE:
-                curr_sum += rectified_1[curr_val]
-                counter += 1
-                curr_val += 1
-            if counter != 0:
-                curr_average = curr_sum / counter
-                rectified_1_bins.append(curr_average)
-        rectified1_zscore = stats.zscore(rectified_1_bins)
-        N = len(val)
-        # sample spacing
+        # graphing fft
+        suplot_plot_pos = 2
+        fft_graphing(axs2, suplot_plot_pos, filtered_val, filtered_rectified1_zscore, 'blue')
 
-        fs = math.ceil(len(val) / 60)
-        T = 1.0 / 800.0
-        NFFT = len(val)
-        # X = fft(x, N)  # compute X[k]
-        # x = ifft(X, N)  # compute x[n]
-        yf = fft(rectified1_zscore)
-        X = fftshift(fft(rectified1_zscore, NFFT))
-        fVals = np.arange(start=-NFFT / 2, stop=NFFT / 2) * fs / NFFT
-        # freq = np.divide(np.fft.fftfreq(N),60)
-        # ft = np.fft.fft(rectified1_zscore)
-        # NFFT = len(val)
-        # fVals = np.arange(start=-NFFT / 2, stop=NFFT / 2) * fs / NFFT
-        # print(len(freq))
-        # axs2[0].plot(fVals, np.divide(2.0, np.multiply(N, np.abs(yf[0:N // 2]))))
-        axs2.plot(fVals, np.abs(X), color = 'red')
+        axs2[0].set(ylabel="DFT Value", title="All Data")
+        axs2[1].set(ylabel="DFT Value", title="Unfiltered Data")
+        axs2[2].set(ylabel="DFT Value", xlabel="Frequency (Hz)", title="Filtered Data")
 
         plt.show()
 
